b484b15b6b52433e743bef3c4504166fe131ab27
Ali Moeeny <alimoeeny@gmail.com>
2022-09-28 09:09:24 -0400

    added daisy ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,12 +8,7 @@ function App() {
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button className="btn btn-outline btn-primary">Button</button>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
